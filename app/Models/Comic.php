722be2baeefc1ac997fb65e11dec21abc7c28531
manuelgaleone <galeonemanuel14@gmail.com>
2022-12-22 14:59:17 +0100

    Fixed edit with model

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
 
-    protected $fillable = ['title'];
+    protected $fillable = ['title', 'thumb', 'price', 'description'];
 
     use HasFactory;
 }
